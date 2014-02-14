c75c891d86ed20412a7ced233a4d43373b7f872a
Jon Ursenbach <jon@ursenba.ch>
2014-02-13 16:01:49 -0800

    Re-enabling the LESS middleware.

--- app/server.js
+++ app/server.js
@@ -13,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
+app.use(require('less-middleware')({ src: path.join(__dirname, 'web') }));
 app.use(express.static(path.join(__dirname, 'web')));
 
 // development only
